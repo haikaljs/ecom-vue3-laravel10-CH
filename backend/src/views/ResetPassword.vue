1b192b12c25656750510dfacd06f05c5858f0ea2
haikaljs <haikaljavascript@gmail.com>
2023-08-02 11:23:15 +0800

    Create layout for guest users

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -1,77 +1,68 @@
 <template>
-    <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-    >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2
-                class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-            >
-                Set new password
-            </h2>
-        </div>
-
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" action="#" method="POST">
-                <div>
-                    <label
-                        for="new-password"
-                        class="block text-sm font-medium leading-6 text-gray-900"
-                        >New password</label
-                    >
-                    <div class="mt-2">
-                        <input
-                            id="new-password"
-                            name="password"
-                            type="password"
-                            required=""
-                            placeholder="New password"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
+    <GuestLayout title="Set new password">
+        <form class="space-y-6" action="#" method="POST">
+            <div>
+                <label
+                    for="new-password"
+                    class="block text-sm font-medium leading-6 text-gray-900"
+                    >New password</label
+                >
+                <div class="mt-2">
+                    <input
+                        id="new-password"
+                        name="password"
+                        type="password"
+                        required=""
+                        placeholder="New password"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                 </div>
+            </div>
 
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="password-repeat"
-                            class="block text-sm font-medium leading-6 text-gray-900"
-                            >Repeat password</label
+            <div>
+                <div class="flex items-center justify-between">
+                    <label
+                        for="password-repeat"
+                        class="block text-sm font-medium leading-6 text-gray-900"
+                        >Repeat password</label
+                    >
+                </div>
+                <div class="mt-2">
+                    <input
+                        id="password-repeat"
+                        name="password_repeat"
+                        type="password"
+                        required=""
+                        placeholder="Repeat password"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                    <div class="text-sm text-right mt-1">
+                        <router-link
+                            :to="{ name: 'login' }"
+                            class="font-semibold text-indigo-600 hover:text-indigo-500"
+                            >Go back to login</router-link
                         >
                     </div>
-                    <div class="mt-2">
-                        <input
-                            id="password-repeat"
-                            name="password_repeat"
-                            type="password"
-                            required=""
-                            placeholder="Repeat password"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                        <div class="text-sm text-right mt-1">
-                            <router-link
-                                :to="{ name: 'requestPassword' }"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >Go back to login</router-link
-                            >
-                        </div>
-                    </div>
                 </div>
+            </div>
 
-                <div>
-                    <button
-                        type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
+            <div>
+                <button
+                    type="submit"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    </GuestLayout>
 </template>
 
 <script>
+import GuestLayout from "../components/GuestLayout.vue";
+
 export default {
     name: "Login",
+    components: { GuestLayout },
 };
 </script>
