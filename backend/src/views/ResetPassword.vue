20bb488fc4d3b0b7deef0c58d6bfbfc15548628f
haikaljs <haikaljavascript@gmail.com>
2023-08-02 08:18:29 +0800

    Style text-right

--- backend/src/views/ResetPassword.vue
+++ backend/src/views/ResetPassword.vue
@@ -47,7 +47,7 @@
                             placeholder="Repeat password"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
-                        <div class="text-sm mt-1">
+                        <div class="text-sm text-right mt-1">
                             <router-link
                                 :to="{ name: 'requestPassword' }"
                                 class="font-semibold text-indigo-600 hover:text-indigo-500"
