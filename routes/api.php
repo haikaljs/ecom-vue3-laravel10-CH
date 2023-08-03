1c8fa6b5659fd1d8c338421b1d128c7259db63ed
haikaljs <haikaljavascript@gmail.com>
2023-08-03 19:29:52 +0800

    Implement login & logout for admin in laravel

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum', 'admin')->group(function() {
+    Route::get('/user', function(Request $request){
+        return $request->user;
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+Route::post('login', [AuthController::class, 'login']);
